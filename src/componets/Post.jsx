7934d6064b473996c532b72152d804ea0deea9eb
carlos_wendel <wendeluzumak824@gmail.com>
2025-01-15 16:17:40 -0300

    date and props manipulation

--- src/componets/Post.jsx
+++ src/componets/Post.jsx
@@ -1,46 +1,58 @@
+import { format, formatDistanceToNow } from 'date-fns';
 import styles from './Post.module.css';
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 
 export function Post(props) {
-    console.log(props)
+    const publishedDateFormated = format(props.publishedAt, "dd 'de' LLLL 'às' HH:mm'h'")
+    const publishedDateRelativeToNow = formatDistanceToNow(props.publishedAt, {
+        addSuffix: true
+    })
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar hasBorder={true} src="https://github.com/CarlosWendel.png" />
+                    <Avatar hasBorder={true} src={props.author.avatarUrl} />
                     <div className={styles.authorInfo}>
                         <strong>
-                            Carlos Wendel
+                            {props.author.name}
                         </strong>
                         <span>
-                            Web Developer
+                            {props.author.role}
                         </span>
                     </div>
                 </div>
-                <time title='09 de janeiro ás 09:36h' datetime="2025-01-09 09:36:00">published 1 hour ago </time>
+                <time title={publishedDateFormated} datetime={props.publishedAt.toISOString()}>
+                    {publishedDateRelativeToNow}
+                </time>
             </header>
             <div className={styles.content}>
-                
+                {props.content.map(line => {
+                    if (line.type == 'paragraph') {
+                        return <p>{line.content}</p>
+                    } else if (line.type == 'link') {
+                        return <p><a href="">{line.content}</a></p>
+                    }
+                })}
             </div>
 
-                <form className={styles.commentForm}>
-                    <strong>
-                        Deixe seu feedback
-                    </strong>
-                    <textarea
-                        placeholder='Deixe um comentário'
-                    />
-                    <footer>
-                        <button type='submit'>Comentário</button>
-                    </footer>
+            <form className={styles.commentForm}>
+                <strong>
+                    Deixe seu feedback
+                </strong>
+                <textarea
+                    placeholder='Deixe um comentário'
+                />
+                <footer>
+                    <button type='submit'>Comentário</button>
+                </footer>
 
-                </form>
-                <div className={styles.commentList}>
-                    <Comment/>
-                    <Comment/>
-                    <Comment/>
-                </div>
+            </form>
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
+            </div>
         </article>
     )
 }
